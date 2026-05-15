57431ba7357c744890d090da02b35589033395cf
hirano <alterna.co3@gmail.com>
2026-05-15 10:58:56 +0900

    Add Firebase configuration fileAdd Firebase config

---
+++ assets/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase 設定
+export const firebaseConfig = {
+  apiKey: "AIzaSyDaMEzmOc393GSdZoKlwUUpTt84nOcDxDQ",
+  authDomain: "btc-signal-ai.firebaseapp.com",
+  projectId: "btc-signal-ai",
+  storageBucket: "btc-signal-ai.firebasestorage.app",
+  messagingSenderId: "1020513313604",
+  appId: "1:1020513313604:web:77979e06b748bf29d62d11"
+};
